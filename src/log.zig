be09477ab046a769c1265d603dfee10650115685
Hariganesh Srinivasan <hariganeshs1999@gmail.com>
2023-10-29 17:31:18 -0400

    feat: add logging

---
+++ src/log.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const console = @import("console.zig");
+
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_txt = comptime message_level.asText();
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    console.printf(level_txt ++ prefix2 ++ format ++ "\n", args);
+}
--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,18 @@ const multiboot = @import("multiboot.zig");
 const ally = @import("ally.zig");
 const layout = @import("layout.zig");
 const debug = @import("debug.zig");
+const log = @import("log.zig");
+
+const boot_logger = std.log.scoped(.boot);
 
 // embedding in file works for non-Debug builds.
 comptime {
     asm (@embedFile("boot.s"));
 }
+
+pub const std_options = struct {
+    pub const logFn = log.log;
+};
 
 const MultibootMmapEntry = multiboot.MultibootMmapEntry;
 
@@ -21,31 +28,17 @@ pub var fixed_buffer: [1024 * 1024]u8 = undefined;
 pub var fixed_buffer_allocator: std.heap.FixedBufferAllocator = std.heap.FixedBufferAllocator.init(fixed_buffer[0..]);
 
 export fn kmain(multiboot_magic: u32, info: *const multiboot.MultibootInfo) void {
+    _ = multiboot_magic;
+
     console.initialize();
+    boot_logger.info("console initialized", .{});
 
-    console.write("magic ::: {x}", .{multiboot_magic});
-    console.write("valid mmap ::: {d}", .{info.flags >> 6 & 0x1});
-    console.write("flags ::: {x}", .{info.flags});
     console.write("bootloader name ::: {s}", .{info.boot_loader_name});
-    console.write("mmap length ::: {d}", .{info.mmap_length});
 
-    console.write("num mmap = {d}", .{info.getNumMmap()});
-
-    var total_len: u32 = 0;
-    for (info.getMmapAddrs()) |mmap| if (mmap.type == multiboot.MULTIBOOT_MEMORY_AVAILABLE) {
-        const size_kb: f32 = @as(f32, @floatFromInt(mmap.len)) / 1024.0;
-        total_len += mmap.len;
-        console.write("size: {d}, len: {d:.2}K, start addr: {x}", .{ mmap.size, size_kb, mmap.addr });
-    };
-    console.write("total mmap len ::: {d:.2}M", .{@as(f32, @floatFromInt(total_len)) / 1024.0 / 1024.0});
-    const kernel_start: u32 = @intFromPtr(&layout.KERNEL_START);
-    const kernel_end: u32 = @intFromPtr(&layout.KERNEL_END);
-    console.write("KERNEL_START = {x}, KERNEL_END = {x}", .{ kernel_start, kernel_end });
-
-    console.write("Module len: {d}", .{info.mods_count});
-
+    boot_logger.info("initializing panic handler and debug symbols", .{});
     debug.initSymbols(info.getModules(), fixed_buffer_allocator.allocator()) catch @panic("Failed to initialize debug symbols.");
 
+    boot_logger.info("initializing memory allocator", .{});
     var alloc = ally.init(info);
     var allocator = alloc.allocator();
     var arr_a = std.ArrayList(u8).initCapacity(allocator, 2) catch @panic("allocating arr");
@@ -58,15 +51,13 @@ export fn kmain(multiboot_magic: u32, info: *const multiboot.MultibootInfo) void
     arr_a.append(5) catch @panic("appending 5");
     arr_a.append(6) catch @panic("appending 6");
 
-    // arr_a.shrinkAndFree(6);
+    const testStruct = struct { u32, u32, u32, u128 };
 
     var ts = allocator.create(testStruct) catch @panic("out of memory");
     defer allocator.destroy(ts);
 
     ts.* = .{ 2, 2, 2, 2 };
 }
-
-const testStruct = struct { u32, u32, u32, u128 };
 
 pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, return_addr: ?u32) noreturn {
     @setCold(true);
